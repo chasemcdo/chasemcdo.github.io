907b6d452bf858ade2866bb0a7450f2934ac2065
BuildTools <chasemcdougall@hotmail.com>
2021-11-07 13:30:26 -0500

    Updates

---
+++ precache-manifest.6cfe31b34b44c102f3f7fd2e39d74c86.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f19f3fdaf711ea08ae425695c9e0e22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4f2d5b3101b01a56750",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "7e4d26148fa2ee43de0f",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "6af2df2c92370e117a1b",
+    "url": "/static/css/main.faa50e85.chunk.css"
+  },
+  {
+    "revision": "d4b325ba474ee3b0e149",
+    "url": "/static/js/2.6cffdce9.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.6cffdce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4f2d5b3101b01a56750",
+    "url": "/static/js/3.b6df7806.chunk.js"
+  },
+  {
+    "revision": "7e4d26148fa2ee43de0f",
+    "url": "/static/js/4.09e78f1c.chunk.js"
+  },
+  {
+    "revision": "6af2df2c92370e117a1b",
+    "url": "/static/js/main.1358e245.chunk.js"
+  },
+  {
+    "revision": "c8524fd974f2e773e5de",
+    "url": "/static/js/runtime-main.743cd1f3.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "90a896b45d355bdb9f1f28d5a8e2162c",
+    "url": "/static/media/RCMPlogo.90a896b4.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "78d4c143d08a376142d016e4667e2562",
+    "url": "/static/media/marks-logo.78d4c143.jpg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "11ec10a5d0e2c70425e500399b47dd97",
+    "url": "/static/media/utorontologo.11ec10a5.png"
+  }
+]);
